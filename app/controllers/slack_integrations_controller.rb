9fc95dcf5b6ebc4bdd4c73c3d35af7497eff9fe7
Aaron Glasenapp <glassbead@gmail.com>
2014-09-22 15:35:31 -0600

    WIP creating a slackIntegration instance after callback

--- app/controllers/slack_integrations_controller.rb
+++ app/controllers/slack_integrations_controller.rb
@@ -1,10 +1,20 @@
 class SlackIntegrationsController < ApplicationController
+
   def connect
   end
 
+  def create
+    @slack_integration = SlackIntegration.new(params[:slack_integrations])
+    @slack_integration.save
+    current_user.slack_integration = @slack_integration
+    redirect_to slack_integrations_test_notifications_path
+  end
+
   def options
+    @slack_integration = SlackIntegration.new
   end
 
   def test_notifications
   end
+
 end
